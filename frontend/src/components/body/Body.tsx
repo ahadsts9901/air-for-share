da973bb808ef703a41135434af66d682f862a0a9
ahadsts9901 <ahadsts990@gmail.com>
2024-10-03 17:50:15 +0500

    sidebar

--- frontend/src/components/body/Body.tsx
+++ frontend/src/components/body/Body.tsx
@@ -1,13 +1,37 @@
-import "./Body.css"
+import { useState } from "react";
+import "./Body.css";
+import { BsTextLeft } from "react-icons/bs";
+import { RxFileText } from "react-icons/rx";
 
 const Body = () => {
-    return (
-        <>
-            <div className="air-body">
-                
-            </div>
-        </>
-    )
-}
+    const [isText, setIsText] = useState(true);
 
-export default Body
+    const sidebarOptions = [
+        {
+            label: <BsTextLeft className="sidebar-icons" />,
+            fun: () => setIsText(true),
+        },
+        {
+            label: <RxFileText className="sidebar-icons" />,
+            fun: () => setIsText(false),
+        },
+    ];
+
+    return (
+        <div className="air-body">
+            <ul className="sidebar">
+                {sidebarOptions.map((opt, i) => (
+                    <li
+                        key={i}
+                        onClick={opt.fun}
+                        className={`${isText === (i === 0) ? "special" : ""}`}
+                    >
+                        {opt.label}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Body;
