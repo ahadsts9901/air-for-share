044f4c4f56fe244284d684f2d89c8914e097c427
ahadsts9901 <ahadsts990@gmail.com>
2024-10-03 17:03:21 +0500

    footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,9 +1,21 @@
 import "./Footer.css"
+import { facebookUrl, githubUrl, linkedinUrl, whatsappUrl, youtubeUrl } from "../../utils/core"
+import { FaFacebook, FaLinkedinIn, FaWhatsapp, FaYoutube, FaGithub } from "react-icons/fa";
 
 const Footer = () => {
     return (
         <>
-            <div className="footer"></div>
+            <div className="footer">
+                <p>2024 - {new Date().getFullYear()} github.com/ahadsts9901</p>
+                <p>Made by <a href={githubUrl}>Muhammad Ahad</a> with ❤️</p>
+                <div className="social-links">
+                    <a href={facebookUrl}><FaFacebook /></a>
+                    <a href={linkedinUrl}><FaLinkedinIn /></a>
+                    <a href={whatsappUrl}><FaWhatsapp /></a>
+                    <a href={youtubeUrl}><FaYoutube /></a>
+                    <a href={githubUrl}><FaGithub /></a>
+                </div>
+            </div>
         </>
     )
 }
---
+++ frontend/src/utils/core.ts
@@ -0,0 +1,5 @@
+export const githubUrl = "https://github.com/ahadsts9901"
+export const linkedinUrl = "https://www.linkedin.com/in/muhammad-ahad-%F0%9F%87%B5%F0%9F%87%B8-3136b1236/"
+export const whatsappUrl = "https://wa.me/+923130019086"
+export const facebookUrl = "https://www.facebook.com/abdulahad.ansari.1481"
+export const youtubeUrl = "https://www.youtube.com/@AhadSTS"
