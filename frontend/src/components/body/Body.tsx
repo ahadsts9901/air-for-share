595a3e41d71b1857dc91bc456a5a0a6e1eea08da
ahadsts9901 <ahadsts990@gmail.com>
2024-10-03 08:42:55 -0700

    text-secion

--- frontend/src/components/body/Body.tsx
+++ frontend/src/components/body/Body.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import "./Body.css";
+import { useState } from "react";
 import { BsTextLeft } from "react-icons/bs";
 import { RxFileText } from "react-icons/rx";
 
-const Body = () => {
-    const [isText, setIsText] = useState(true);
+export const Sidebar = ({ isText, setIsText }: any) => {
 
     const sidebarOptions = [
         {
@@ -18,7 +17,7 @@ const Body = () => {
     ];
 
     return (
-        <div className="air-body">
+        <>
             <ul className="sidebar">
                 {sidebarOptions.map((opt, i) => (
                     <li
@@ -30,6 +29,68 @@ const Body = () => {
                     </li>
                 ))}
             </ul>
+        </>
+    )
+
+}
+
+export const TextSection = () => {
+
+    const [text, set_text] = useState("")
+    const [button_content, set_button_content] = useState("Save")
+
+    const clear = () => {
+        console.log("clear");
+    }
+
+    const handleClick = () => {
+        console.log("handle click")
+    }
+
+    return (
+        <>
+            <div className="text-section section">
+                <h3>Text</h3>
+                <textarea placeholder="Type something..." onChange={(e: any) => set_text(e?.target?.value)}></textarea>
+                <div className="buttons-cont">
+                    <button className="clearButton" onClick={clear}>Clear</button>
+                    <button className="saveButton" onClick={handleClick}>{button_content}</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const FileSection = () => {
+    return (
+        <>
+            <div className="text-section section">
+                <h3>File</h3>
+            </div>
+        </>
+    )
+}
+
+export const Main = ({ isText }: any) => {
+
+    return (
+        <>
+            <div className="_main">
+                {isText ? <TextSection /> : <FileSection />}
+            </div>
+        </>
+    )
+
+}
+
+const Body = () => {
+
+    const [isText, setIsText] = useState(true);
+
+    return (
+        <div className="air-body">
+            <Sidebar isText={isText} setIsText={setIsText} />
+            <Main isText={isText} setIsText={setIsText} />
         </div>
     );
 };
