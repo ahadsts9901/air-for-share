2c352ddbeeb7a34a031118c34ae728faa3b987d6
ahadsts9901 <ahadsts990@gmail.com>
2024-10-03 09:17:58 -0700

    example-data

---
+++ backend/dummy-data/data.mjs
@@ -0,0 +1,14 @@
+export const dataExample = {
+    textData: {
+        text: "string"
+    },
+    fileData: {
+        filePath: "string",
+        filename: "string",
+        fileSize: "number"
+    },
+    location: {
+        latitude: "number",
+        longitude: "number"
+    }
+}
