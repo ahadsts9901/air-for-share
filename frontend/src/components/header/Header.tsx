620facdff54311a9031439ad4bacc2d8bd0c461c
ahadsts9901 <ahadsts990@gmail.com>
2024-10-03 17:15:09 +0500

    header-footer

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -3,29 +3,33 @@ import logo from "/logo.svg"
 
 const Logo = () => {
 
-    // const navbarOptions = [
-    //     {
-    //         label: "How it works",
-    //         path: "/",
-    //     },
-    //     {
-    //         label: "Download",
-    //         path: "/",
-    //     },
-    //     {
-    //         label: "Upgrade",
-    //         path: "/",
-    //     },
-    //     {
-    //         label: "Feedback",
-    //         path: "/"
-    //     }
-    // ]
+    const navbarOptions = [
+        {
+            label: "How it works",
+            path: "/",
+        },
+        {
+            label: "Download",
+            path: "/",
+        },
+        {
+            label: "Upgrade",
+            path: "/",
+        },
+        {
+            label: "Feedback",
+            path: "/"
+        }
+    ]
 
     return (
         <>
             <div className="header">
-                <img src={logo} alt="logo" />
+                <img src={logo} alt="logo" onClick={() => window.location.reload()} />
+                <ul>
+                    {navbarOptions.map((opt: any, i: number) => <li key={i} onClick={() => window.location.href = "#"}>{opt?.label}</li>)}
+                    <li className="login-register">Login/Register</li>
+                </ul>
             </div>
         </>
     )
